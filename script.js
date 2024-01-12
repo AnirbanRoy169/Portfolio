8ae19c5a8214e1b5cf41b1a0d7202b1bcbb25f75
Anirban Roy <156198466+AnirbanRoy169@users.noreply.github.com>
2024-01-12 19:50:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#element', {
+    // strings: ['Web Developer', 'Software Engineer', 'Physics Teacher', 'Animator', 'Content Creator', 'Flute Artist', 'Vfx Artist', 'Trader'],
+    strings: ['Web Developer', 'Software Developer'],
+    typeSpeed: 50,
+    loop: true,
+    backDelay: 900,
+    backSpeed: 30,
+});
